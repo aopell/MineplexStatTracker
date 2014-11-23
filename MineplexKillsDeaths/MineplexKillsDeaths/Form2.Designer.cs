6030d2df6f81bdd8994c27df39f6471b6fa23db6
Aaron Opell <aaron.opell@live.com>
2014-11-23 11:01:05 -0800

    updated icon

--- MineplexKillsDeaths/MineplexKillsDeaths/Form2.Designer.cs
+++ MineplexKillsDeaths/MineplexKillsDeaths/Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -141,6 +142,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
             this.Text = "Settings";
             this.Load += new System.EventHandler(this.Form2_Load);
